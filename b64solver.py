fc275b9239163d942a2c70bf874ae2c1fced01de
LordArianz <morteza758@gmail.com>
2020-01-25 20:56:27 +0330

    test single image

--- b64solver.py
+++ b64solver.py
@@ -1,4 +1,4 @@
-from image import get_letters_b64
+from image import get_letters_b64, get_letters
 from config import CONFIGURATION
 from data import build_captcha_data
 import os
@@ -6,33 +6,46 @@ import numpy as np
 import tensorflow as tf
 from model import build_model
 from helper import prediction_to_captcha
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--fname', default='data/captcha/2ahckd.jpg',  help='captcha image path')
 
 
-b64 = input()
+def main(argv):
+    args = parser.parse_args(argv[1:])
+    if args.fname == None:
+        print('Capture Image is missing!')
+        return None
+    fname = args.fname
 
-data_x = []
+    data_x = []
 
-letters = get_letters_b64(b64)
-if letters != None:
-    X = []
-    for _, letter in enumerate(letters):
-        heigth, width = np.shape(letter)
-        if heigth != CONFIGURATION['data']['height'] or width != CONFIGURATION['data']['width']:
-            continue
-        X.append(letter)
-    if len(X) == len(letters):
-        data_x.append(X)
+    letters = get_letters(fname)
+    if letters != None:
+        X = []
+        for _, letter in enumerate(letters):
+            heigth, width = np.shape(letter)
+            if heigth != CONFIGURATION['data']['height'] or width != CONFIGURATION['data']['width']:
+                continue
+            X.append(letter)
+        if len(X) == len(letters):
+            data_x.append(X)
 
-checkpoint_dir = os.path.dirname(CONFIGURATION['path']['checkpoint'])
-latest = tf.train.latest_checkpoint(checkpoint_dir)
+    checkpoint_dir = os.path.dirname(CONFIGURATION['path']['checkpoint'])
+    latest = tf.train.latest_checkpoint(checkpoint_dir)
 
-model = build_model()
-model.load_weights(latest).expect_partial()
+    model = build_model()
+    model.load_weights(latest).expect_partial()
 
-for i, data in enumerate(data_x):
-    prediction = []
-    for d in data:
-        d = d.reshape([-1, 20, 20, 1])
-        prediction.append(tf.argmax(model.predict(d), 1).numpy()[0])
-    answer = prediction_to_captcha(prediction)
-    print(answer)
+    for i, data in enumerate(data_x):
+        prediction = []
+        for d in data:
+            d = d.reshape([-1, 20, 20, 1])
+            prediction.append(tf.argmax(model.predict(d), 1).numpy()[0])
+        answer = prediction_to_captcha(prediction)
+        print(answer)
+
+
+if __name__ == '__main__':
+    tf.compat.v1.app.run(main)
