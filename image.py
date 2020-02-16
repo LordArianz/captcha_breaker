d66148ec81b041cfe1d6a28555dadda292e9bca9
LordArianz <morteza758@gmail.com>
2020-02-16 10:31:33 +0330

    b64 added

--- image.py
+++ image.py
@@ -4,6 +4,7 @@ from config import CONFIGURATION
 import base64
 from PIL import ImageFont, ImageDraw, Image
 from io import StringIO, BytesIO
+import matplotlib.pyplot as plt
 
 
 def mask(img, low, high, new_color=(0, 0, 0)):
@@ -15,22 +16,42 @@ def mask(img, low, high, new_color=(0, 0, 0)):
     img[mask > 0] = new_color
     return img
 
-def split_letters(raw_image, num_letters=6):
+def split_letters(raw_image, num_letters=6, verbose=False):
+    if verbose:
+        cv2.imwrite('data/tmp/raw_image.jpg', raw_image)
     raw_image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2RGB)
 
     mask(raw_image, [200, 150, 0], [255, 190, 50], (255, 255, 255))
     mask(raw_image, [200, 0, 0], [255, 80, 80])
     mask(raw_image, [0, 100, 0], [100, 200, 100])
 
+    if verbose:
+        cv2.imwrite('data/tmp/masked_image.jpg', raw_image)
+
     gray_image = cv2.cvtColor(raw_image, cv2.COLOR_RGB2GRAY)
+
+    if verbose:
+        cv2.imwrite('data/tmp/gray_image.jpg', gray_image)
 
     blur_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
 
+    if verbose:
+        cv2.imwrite('data/tmp/blured_image.jpg', blur_image)
+
     weighted_image = cv2.addWeighted(gray_image, 1.80, blur_image, -0.55, 0)
+
+    if verbose:
+        cv2.imwrite('data/tmp/weighted_image.jpg', weighted_image)
 
     not_image = cv2.bitwise_not(weighted_image)
 
+    if verbose:
+        cv2.imwrite('data/tmp/not_image.jpg', not_image)
+
     _, thresh_image = cv2.threshold(not_image, 150, 255, cv2.THRESH_BINARY)
+
+    if verbose:
+        cv2.imwrite('data/tmp/thresh_image.jpg', thresh_image)
 
     height, width = np.shape(thresh_image)
     points = np.zeros((int(cv2.sumElems(thresh_image)[0]), 2))
@@ -48,7 +69,7 @@ def split_letters(raw_image, num_letters=6):
     attempts = 10
     _, _, centers = cv2.kmeans(points, num_letters + 1, None, criteria, attempts, cv2.KMEANS_PP_CENTERS)
 
-    # seg = thresh_image.copy()
+    seg = thresh_image.copy()
     centers = sorted(centers[1:], key=lambda x: x[1])
     letters = []
     for center in centers:
@@ -59,8 +80,18 @@ def split_letters(raw_image, num_letters=6):
         
         letter = thresh_image[y - hl_y: y + hl_y, x - hl_x: x + hl_x].copy()
         letters.append(letter)
-        # seg = cv2.circle(seg, (x, y), 2, (255, 0, 0))
-        # seg = cv2.rectangle(seg, (x - hl_x, y - hl_y), (x + hl_x, y + hl_y), (255, 0, 0))
+        seg = cv2.circle(seg, (x, y), 2, (255, 0, 0))
+        seg = cv2.rectangle(seg, (x - hl_x, y - hl_y), (x + hl_x, y + hl_y), (255, 0, 0))
+
+    if verbose:
+        cv2.imwrite('data/tmp/splited_image.jpg', seg)
+        hist = np.zeros(width)
+        for i in range(len(hist)):
+            hist[i] = cv2.countNonZero(thresh_image[:, i])
+
+        plt.plot(hist)
+        plt.show()
+
 
     return letters
 
@@ -71,9 +102,9 @@ def readb64(b64):
     return cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
 
 
-def get_letters(fname, num_letters=6):
+def get_letters(fname, num_letters=6, verbose=False):
     raw_image = cv2.imread(fname)
-    return split_letters(raw_image, num_letters)
+    return split_letters(raw_image, num_letters, verbose)
 
 
 def get_letters_b64(b64, num_letters=6):
